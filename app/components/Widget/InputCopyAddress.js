bd67c25100e32afc62ee529a6286952faff06865
Louis <congnt@hvtc.edu.vn>
2020-02-20 16:01:07 +0700

    updateData

--- app/components/Widget/InputCopyAddress.js
+++ app/components/Widget/InputCopyAddress.js
@@ -1,95 +1,102 @@
 /* eslint-disable */
-import React, { useState, useContext, useEffect } from 'react'
-import PapperBlock from '../PapperBlock/PapperBlock'
-import { Input, withStyles, Button, SvgIcon, TextField, FormControl, Tooltip } from '@material-ui/core'
+import React, { useState, useContext, useEffect } from "react";
+import PapperBlock from "../PapperBlock/PapperBlock";
+import {
+  Input,
+  withStyles,
+  Button,
+  SvgIcon,
+  TextField,
+  FormControl,
+  Tooltip
+} from "@material-ui/core";
 import styles from "../Tables/tableStyle-jss";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { Web3Context } from "../../../provider/web3";
-import styled from 'styled-components';
-import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+import styled from "styled-components";
+import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
 
 const InputCopyAddress = ({ classes }) => {
-    const [value, setValue] = useState('');
-    const [copy, setCopy] = useState(false)
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    }
-    const { t, i18n } = useTranslation();
-    const textTranslate = text => {
-        return i18n.exists(text) ? t(text) : text;
-    };
-    const { address } = useContext(Web3Context)
-    // console.log('address', address)
-    useEffect(() => {
-        setValue(address)
-    }, [address])
-    useEffect(() => {
-        const time = setTimeout(() => { setCopy(false); }, 1500);
-        return () => clearTimeout(time)
-    }, [copy])
-    const domain = window.location.href + "?ref="
-    return (
-        <PapperBlock
-            className={classes.root}
-            noMargin
-            title={textTranslate("address")}
-            icon="ios-basket-outline"
-            whiteBg
-            desc=""
-        >
-            <Wrap>
-                {/* <Input value={value} fullWidth readOnly /> */}
-                <FormControl fullWidth className={classes.formControlTrade}>
-                    <Input
-                        id="adornment-amount1"
-                        value={value}
-                        disabled
-                        startAdornment={(
-                            <InputAdornment position="start">
-                                <ImportContactsIcon />
-                            </InputAdornment>
-                        )}
-                    />
-                </FormControl>
-                <WrapCopy>
-                    <Button onClick={() => { navigator.clipboard.writeText(domain + value); setCopy(true) }}>
-                        <Tooltip title="copy">
-                            <FileCopyOutlinedIcon />
-                        </Tooltip>
-                    </Button>
-                    {copy && <Copied>Copied!</Copied>}
-                </WrapCopy>
-            </Wrap>
-        </PapperBlock>
-    )
-}
+  const [copy, setCopy] = useState(false);
+  const { t, i18n } = useTranslation();
+  const textTranslate = text => {
+    return i18n.exists(text) ? t(text) : text;
+  };
+  const { address } = useContext(Web3Context);
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setCopy(false);
+    }, 1500);
+    return () => clearTimeout(time);
+  }, [copy]);
+  const domain = window.location.href + "?ref=";
+  return (
+    <PapperBlock
+      className={classes.root}
+      noMargin
+      title={textTranslate("refLink")}
+      icon="ios-basket-outline"
+      whiteBg
+      desc=""
+    >
+      <Wrap>
+        {/* <Input value={value} fullWidth readOnly /> */}
+        <FormControl fullWidth className={classes.formControlTrade}>
+          <Input
+            id="adornment-amount1"
+            value={domain + address}
+            disabled
+            startAdornment={
+              <InputAdornment position="start">
+                <ImportContactsIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <WrapCopy>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(domain + address);
+              setCopy(true);
+            }}
+          >
+            <Tooltip title="copy">
+              <FileCopyOutlinedIcon />
+            </Tooltip>
+          </Button>
+          {copy && <Copied>Copied!</Copied>}
+        </WrapCopy>
+      </Wrap>
+    </PapperBlock>
+  );
+};
 
-export default withStyles(styles)(InputCopyAddress)
+export default withStyles(styles)(InputCopyAddress);
 const Wrap = styled.div`
-    display:flex;
-    /* flex-flow:row wrap; */
-    justify-content:space-between;
-    align-items:center;
-    position:relative;
-    input{
-        width:100%;
-    }
-    .MuiInputAdornment-positionStart{
-        margin-right: 10px;
-        margin-left: 10px;
-        margin-bottom: 7px;
-    }
-`
+  display: flex;
+  /* flex-flow:row wrap; */
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  input {
+    width: 100%;
+  }
+  .MuiInputAdornment-positionStart {
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-bottom: 7px;
+  }
+`;
 const WrapCopy = styled.div`
-    position: relative;
-`
+  position: relative;
+`;
 const Copied = styled.div`
-    position:absolute;
-    color:#111;
-    bottom:-20px;
-    left:0;
-    text-align:center;
-    width:100%;
-`
+  position: absolute;
+  color: #111;
+  bottom: -20px;
+  left: 0;
+  text-align: center;
+  width: 100%;
+`;
