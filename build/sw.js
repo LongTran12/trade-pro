08aaaa64d87c2bd8306db9b2f3f37e475ff3c62f
Louis <congnt@hvtc.edu.vn>
2020-03-26 11:22:50 +0700

    new contract

--- build/sw.js
+++ build/sw.js
@@ -69,7 +69,7 @@ var __wpo = {
       "/13.82064950731b0bfb51e2.chunk.js",
       "/14.de62ee6d13f83f00d6cd.chunk.js",
       "/15.65124c9c75963118844e.chunk.js",
-      "/main.6315c9e3bd70f388cf51.chunk.js",
+      "/main.921a0355296cb4ab0b24.chunk.js",
       "/18.9154994ab085193275f3.chunk.js",
       "/19.708fb811ae5e2c305feb.chunk.js",
       "/20.a7f5ab5bc6360993a566.chunk.js",
@@ -243,7 +243,7 @@ var __wpo = {
     "a9ffca16afb8484be9066a81d5d988ae27b6492d": "/13.82064950731b0bfb51e2.chunk.js",
     "4d7d3c166eb36c64694d1f6c567b074c69a4dc95": "/14.de62ee6d13f83f00d6cd.chunk.js",
     "84c4b0c13f71bce3770b08fd532cd9ac30b7cf31": "/15.65124c9c75963118844e.chunk.js",
-    "feec9e1ef37a8c9bff7d2093b684332c336f769d": "/main.6315c9e3bd70f388cf51.chunk.js",
+    "d157224703b46cb40e576a32c50068c5e4a640ca": "/main.921a0355296cb4ab0b24.chunk.js",
     "338b30007dc8c35d8e97cca7631355b8defde711": "/runtime~main.d434a03d2bae5e087c44.js",
     "d79f24e2af433ce07dfb5bc16a5e71cc6a598916": "/18.9154994ab085193275f3.chunk.js",
     "a3b17d860492eb6b91b633cd2a69a18c2adece46": "/19.708fb811ae5e2c305feb.chunk.js",
@@ -349,11 +349,11 @@ var __wpo = {
     "85533bdd1150f62edb43cdcea0f40f14dc4fbf68": "/119.668b2eee7ee04d0f3c8b.chunk.js",
     "7f6a4c96f2550592b6a05a5a49e8d98d8039e3de": "/120.0d458c265f85226fd3cf.chunk.js",
     "9c8b698f838971b592d82e00cb658c929f5ec828": "/121.bc6ceb2106f747436403.chunk.js",
-    "1bd170cb633fb4852d99d0a482d1433b59a63eb4": "/"
+    "89d50a2714583741832edbb66dcecac9c5f18945": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "3/25/2020, 11:08:18 AM",
+  "version": "3/26/2020, 11:21:46 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
