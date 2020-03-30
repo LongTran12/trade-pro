221bd2c73ea99cd034d6b5f15503105b01feb25c
LongTran12 <tranlong12111995@gmail.com>
2020-03-30 15:47:11 +0700

    Edit all Logo

--- app/api/images/avatars.js
+++ app/api/images/avatars.js
@@ -5,6 +5,7 @@ const avatars = [
   '/images/avatars/pp_girl3.svg',
   '/images/avatars/pp_girl4.svg',
   '/images/avatars/pp_girl5.svg',
+  '/images/avatars/coinUser.png',
   '/images/avatars/pp_boy.svg',
   '/images/avatars/pp_boy2.svg',
   '/images/avatars/pp_boy3.svg',
