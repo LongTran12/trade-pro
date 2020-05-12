05a837411186f9fb7910ed5d477e8f026c1da1fa
Louis <congnt@hvtc.edu.vn>
2020-05-12 13:52:51 +0700

    edit bo

--- app/api/ui/menu.js
+++ app/api/ui/menu.js
@@ -1,196 +1,196 @@
 module.exports = [
-    {
-        key: "home",
-        name: "home",
-        icon: "ios-home-outline",
-        child: [
-            {
-                key: "crypto",
-                name: "Cryptocurrency",
-                link: "/",
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "network",
-                name: "network",
-                link: "/network",
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "stackingMenu",
-                name: "stakingMenu",
-                link: "/stacking",
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "calendar",
-                name: "Calendar",
-                link: "/pages/calendar",
-                icon: "ios-calendar-outline",
-            },
-        ],
-    },
+  {
+    key: "home",
+    name: "home",
+    icon: "ios-home-outline",
+    child: [
+      {
+        key: "crypto",
+        name: "Cryptocurrency",
+        link: "/",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "network",
+        name: "network",
+        link: "/network",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "stackingMenu",
+        name: "stakingMenu",
+        link: "/stacking",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "calendar",
+        name: "Calendar",
+        link: "/pages/calendar",
+        icon: "ios-calendar-outline",
+      },
+    ],
+  },
 
-    {
-        key: "otgame",
-        name: "OTGame",
-        icon: "md-game-controller-b",
-        child: [
-            {
-                key: "crypto1",
-                name: "Game Lucky",
-                link: "/game-lucky",
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "network1",
-                name: "Binary Option",
-                link: "/coming-soon",
-                icon: "ios-medal-outline",
-            },
-        ],
-    },
-    {
-        key: "trao-doi",
-        name: "swap",
-        icon: "ios-clock-outline",
-        child: [
-            {
-                key: "swap-usdt",
-                name: "swapUSDT",
-                link: "/coming-soon",
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "witdraw",
-                name: "Witdraw",
-                link: "/coming-soon",
+  {
+    key: "otgame",
+    name: "OTGame",
+    icon: "md-game-controller-b",
+    child: [
+      {
+        key: "crypto1",
+        name: "Game Lucky",
+        link: "/coming-soon",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "network1",
+        name: "Binary Option",
+        link: "/bo",
+        icon: "ios-medal-outline",
+      },
+    ],
+  },
+  {
+    key: "trao-doi",
+    name: "swap",
+    icon: "ios-clock-outline",
+    child: [
+      {
+        key: "swap-usdt",
+        name: "swapUSDT",
+        link: "/coming-soon",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "witdraw",
+        name: "Witdraw",
+        link: "/coming-soon",
 
-                icon: "ios-medal-outline",
-            },
-            {
-                key: "ottrade",
-                name: "OTTrade",
-                link: "/coming-soon",
+        icon: "ios-medal-outline",
+      },
+      {
+        key: "ottrade",
+        name: "OTTrade",
+        link: "/coming-soon",
 
-                icon: "ios-card",
-            },
-        ],
-    },
-    {
-        key: "apps",
-        name: "applications",
-        icon: "ios-appstore-outline",
-        child: [
-            {
-                key: "communication_apps",
-                name: "communication",
-                title: true,
-            },
-            {
-                key: "contact",
-                name: "contact",
-                link: "/pages/contact",
-                icon: "ios-contact-outline",
-            },
-            {
-                key: "email",
-                name: "Email",
-                link: "/pages/email",
-                badge: "2",
-                icon: "ios-mail-outline",
-            },
-            {
-                key: "social_apps",
-                name: "social",
-                title: true,
-            },
-            {
-                key: "timeline",
-                name: "Timeline",
-                link: "/pages/timeline",
-                icon: "ios-people-outline",
-            },
-            {
-                key: "chat",
-                name: "Chat",
-                link: "/pages/chat",
-                badge: "4",
-                icon: "ios-chatbubbles-outline",
-            },
-            {
-                key: "ecommerce_app",
-                name: "Ecommerce",
-                title: true,
-            },
-            {
-                key: "productCatalogues",
-                name: "productCatalogues",
-                link: "/pages/ecommerce",
-                icon: "ios-apps-outline",
-            },
-            {
-                key: "item_detail",
-                name: "productDetail",
-                link: "/pages/product-details/1",
-                icon: "ios-card",
-            },
-            {
-                key: "checkoutPage",
-                name: "checkoutPage",
-                link: "/pages/checkout",
-                icon: "ios-cart-outline",
-            },
-            {
-                key: "dynamic_invoice",
-                name: "dynamicInvoice",
-                link: "/pages/invoice",
-                icon: "ios-document-outline",
-            },
-            {
-                key: "productivity_app",
-                name: "Productivity",
-                title: true,
-            },
-            {
-                key: "task",
-                name: "taskBoard",
-                link: "/pages/taskboard",
-                icon: "ios-checkmark-circle-outline",
-            },
-            {
-                key: "ho-so",
-                name: "Hồ Sơ",
-                link: "/coming-soon",
-                icon: "ios-cloud",
-            },
-        ],
-    },
-    {
+        icon: "ios-card",
+      },
+    ],
+  },
+  {
+    key: "apps",
+    name: "applications",
+    icon: "ios-appstore-outline",
+    child: [
+      {
+        key: "communication_apps",
+        name: "communication",
+        title: true,
+      },
+      {
+        key: "contact",
+        name: "contact",
+        link: "/pages/contact",
+        icon: "ios-contact-outline",
+      },
+      {
+        key: "email",
+        name: "Email",
+        link: "/pages/email",
+        badge: "2",
+        icon: "ios-mail-outline",
+      },
+      {
+        key: "social_apps",
+        name: "social",
+        title: true,
+      },
+      {
+        key: "timeline",
+        name: "Timeline",
+        link: "/pages/timeline",
+        icon: "ios-people-outline",
+      },
+      {
+        key: "chat",
+        name: "Chat",
+        link: "/pages/chat",
+        badge: "4",
+        icon: "ios-chatbubbles-outline",
+      },
+      {
+        key: "ecommerce_app",
+        name: "Ecommerce",
+        title: true,
+      },
+      {
+        key: "productCatalogues",
+        name: "productCatalogues",
+        link: "/pages/ecommerce",
+        icon: "ios-apps-outline",
+      },
+      {
+        key: "item_detail",
+        name: "productDetail",
+        link: "/pages/product-details/1",
+        icon: "ios-card",
+      },
+      {
+        key: "checkoutPage",
+        name: "checkoutPage",
+        link: "/pages/checkout",
+        icon: "ios-cart-outline",
+      },
+      {
+        key: "dynamic_invoice",
+        name: "dynamicInvoice",
+        link: "/pages/invoice",
+        icon: "ios-document-outline",
+      },
+      {
+        key: "productivity_app",
+        name: "Productivity",
+        title: true,
+      },
+      {
+        key: "task",
+        name: "taskBoard",
+        link: "/pages/taskboard",
+        icon: "ios-checkmark-circle-outline",
+      },
+      {
+        key: "ho-so",
+        name: "Hồ Sơ",
+        link: "/coming-soon",
+        icon: "ios-cloud",
+      },
+    ],
+  },
+  {
+    key: "admin",
+    name: "Admin",
+    icon: "md-person",
+    link: "/admin-page",
+    admin: "admin",
+    child: [
+      {
         key: "admin",
         name: "Admin",
         icon: "md-person",
         link: "/admin-page",
-        admin: "admin",
-        child: [
-            {
-                key: "admin",
-                name: "Admin",
-                icon: "md-person",
-                link: "/admin-page",
-            },
-            {
-                key: "allMember",
-                name: "All Member",
-                icon: "md-person",
-                link: "/all-member",
-            },
-            {
-                key: "bonuss",
-                name: "Bonus",
-                link: "/bonus",
-                icon: "ios-medal-outline",
-            },
-        ],
-    },
+      },
+      {
+        key: "allMember",
+        name: "All Member",
+        icon: "md-person",
+        link: "/all-member",
+      },
+      {
+        key: "bonuss",
+        name: "Bonus",
+        link: "/bonus",
+        icon: "ios-medal-outline",
+      },
+    ],
+  },
 ];
--- app/containers/Dashboard/GameLucky.js
+++ app/containers/Dashboard/GameLucky.js
@@ -1,172 +1,173 @@
-import React, { useState } from 'react'
-import Slider from 'react-slick';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import imgData from 'dan-api/images/imgData';
-import 'dan-styles/vendors/slick-carousel/slick-carousel.css';
-import 'dan-styles/vendors/slick-carousel/slick.css';
-import 'dan-styles/vendors/slick-carousel/slick-theme.css';
-import styled from 'styled-components';
-import Iframe from 'react-iframe'
-
+import React, { useState } from "react";
+import Slider from "react-slick";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import imgData from "dan-api/images/imgData";
+import "dan-styles/vendors/slick-carousel/slick-carousel.css";
+import "dan-styles/vendors/slick-carousel/slick.css";
+import "dan-styles/vendors/slick-carousel/slick-theme.css";
+import styled from "styled-components";
+import Iframe from "react-iframe";
 
 const carouselData = [
-    {
-        title: "Wefinex",
-        link: "https://wefinex.net/reg?r=4ADE545",
-        img: `https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/wfn.png`,
-    },
-    {
-        title: "NagaTrade",
-        link: "https://trade.nagatrade.com/authorize/register?ref=5ea284d2a2170",
-        img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
-    },
-    {
-        title: "Youtube",
-        youtube: true,
-        link:
-            "https://www.youtube.com/embed/Zmh4oRhRh08",
-        img:
-            "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
-    },
-    {
-        title: "NagaTrade",
-        link: "https://trade.nagatrade.com/authorize/register?ref=5ea284d2a2170",
-        img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
-    },
-    {
-        title: "Youtube",
-        youtube: true,
-        link:
-            "https://www.youtube.com/embed/Zmh4oRhRh08",
-        img:
-            "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
-    }
+  {
+    title: "Wefinex",
+    link: "https://wefinex.net/reg?r=4ADE545",
+    img: `https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/wfn.png`,
+  },
+  // {
+  //   title: "NagaTrade",
+  //   link: "https://trade.nagatrade.com/authorize/register?ref=5ea284d2a2170",
+  //   img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
+  // },
+  {
+    title: "Youtube",
+    youtube: true,
+    link: "https://www.youtube.com/embed/Zmh4oRhRh08",
+    img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
+  },
+  {
+    title: "Wefinex",
+    link: "https://wefinex.net/reg?r=4ADE545",
+    img: `https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/wfn.png`,
+  },
+  // {
+  //   title: "NagaTrade",
+  //   link: "https://trade.nagatrade.com/authorize/register?ref=5ea284d2a2170",
+  //   img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
+  // },
+  {
+    title: "Youtube",
+    youtube: true,
+    link: "https://www.youtube.com/embed/Zmh4oRhRh08",
+    img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
+  },
 ];
-const styles = ({
-    item: {
-        textAlign: 'center',
-        '& img': {
-            margin: '10px auto'
-        }
-    }
-});
+const styles = {
+  item: {
+    textAlign: "center",
+    "& img": {
+      margin: "10px auto",
+    },
+  },
+};
 const GameLucky = ({ classes }) => {
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: false,
-        autoplay: false,
-        focusOnSelect: true
-
-    };
-    const [link, setLink] = useState('')
-    return (
-        <Wrap className="container">
-            <Slider {...settings} >
-                {
-                    carouselData.map((item, index) => {
-                        return (
-                            <>
-                                <div key={index.toString()} className={classes.item} onClick={() => setLink(item.link)} >
-
-                                    {
-                                        item.youtube ?
-                                            <div className="video">
-                                                <Iframe
-                                                    style={{ width: "100%", height: "100%" }}
-                                                    url={item.link}
-                                                    width="100%"
-                                                    height="100%"
-                                                    id="myId"
-                                                    className="myClassname"
-                                                    display="initial"
-                                                    position="relative"
-                                                    frameBorder={0}
-                                                    onClick={() => { setLink(item.link) }}
-                                                />
-                                            </div>
-                                            :
-                                            <img src={item.img} alt={item.title} />
-                                    }
-                                </div>
-                            </>
-                        )
-                    })
-                }
-            </Slider>
-            {!(link === '') &&
-                <Row>
-                    <Column>
-                        <Iframe
-                            url={link}
-                            width="100%"
-                            height="100%"
-                            id="myId"
-                            className="myClassname"
-                            display="initial"
-                            position="relative"
-                            frameBorder={0}
-                        />
-                        <div className="close" onClick={() => setLink('')}>X</div>
-                    </Column>
-                </Row>
-            }
-        </Wrap>
-    );
-}
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: false,
+    autoplay: false,
+    focusOnSelect: true,
+  };
+  const [link, setLink] = useState("");
+  return (
+    <Wrap className="container">
+      <Slider {...settings}>
+        {carouselData.map((item, index) => {
+          return (
+            <>
+              <div
+                key={index.toString()}
+                className={classes.item}
+                onClick={() => setLink(item.link)}
+              >
+                {item.youtube ? (
+                  <div className="video">
+                    <Iframe
+                      style={{ width: "100%", height: "100%" }}
+                      url={item.link}
+                      width="100%"
+                      height="100%"
+                      id="myId"
+                      className="myClassname"
+                      display="initial"
+                      position="relative"
+                      frameBorder={0}
+                      onClick={() => {
+                        setLink(item.link);
+                      }}
+                    />
+                  </div>
+                ) : (
+                  <img src={item.img} alt={item.title} />
+                )}
+              </div>
+            </>
+          );
+        })}
+      </Slider>
+      {!(link === "") && (
+        <Row>
+          <Column>
+            <Iframe
+              url={link}
+              width="100%"
+              height="100%"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+              frameBorder={0}
+            />
+            <div className="close" onClick={() => setLink("")}>
+              X
+            </div>
+          </Column>
+        </Row>
+      )}
+    </Wrap>
+  );
+};
 GameLucky.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(GameLucky)
+export default withStyles(styles)(GameLucky);
 const Wrap = styled.div`
-
-    .video{
-        margin-top:10px;
-        iframe{
-            height:308px;
-            @media (max-width:1280px){
-                height:188px;
-            }
-            @media (max-width:1024px){
-                height:82px;
-            }
-            @media (max-width:991px){
-                height:146px;
-            }
-            @media (max-width:480px){
-                height:82px;
-            }
-        }
+  .video {
+    margin-top: 10px;
+    iframe {
+      height: 308px;
+      @media (max-width: 1280px) {
+        height: 188px;
+      }
+      @media (max-width: 1024px) {
+        height: 82px;
+      }
+      @media (max-width: 991px) {
+        height: 146px;
+      }
+      @media (max-width: 480px) {
+        height: 82px;
+      }
     }
-`
+  }
+`;
 
 const Row = styled.div`
-    position: relative;
-    margin-top:70px;
-`
+  position: relative;
+  margin-top: 70px;
+`;
 const Column = styled.div`
-    width:100%;
-    height:100vh;
-    .close{
-        position: absolute;
-        right:0;
-        top:-50px;
-        font-size:30px;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        width:45px;
-        height:45px;
-        background:#fff;
-        color:#111;
-        border-radius:999px;
-        box-shadow:0 1px 1px rgba(0,0,0,.3);
-    }
-    
-`
+  width: 100%;
+  height: 100vh;
+  .close {
+    position: absolute;
+    right: 0;
+    top: -50px;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 45px;
+    height: 45px;
+    background: #fff;
+    color: #111;
+    border-radius: 999px;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3);
+  }
+`;
