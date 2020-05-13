11fba6170558bfd46720a293c49e3a7f82f7c839
Louis <congnt@hvtc.edu.vn>
2020-05-13 11:43:16 +0700

    build

--- app/containers/Dashboard/GameLucky.js
+++ app/containers/Dashboard/GameLucky.js
@@ -8,119 +8,121 @@ import "dan-styles/vendors/slick-carousel/slick.css";
 import "dan-styles/vendors/slick-carousel/slick-theme.css";
 import styled from "styled-components";
 import Iframe from "react-iframe";
-import imgBinance from "dan-images/bo/BinanceFuture1.png"
-import imgExness from "dan-images/bo/exness1.png"
-import imgNaga from "dan-images/bo/naga1.png"
-import imgWefinex from "dan-images/bo/wefinex1.png"
+import imgBinance from "dan-images/bo/BinanceFuture1.png";
+import imgExness from "dan-images/bo/exness1.png";
+import imgNaga from "dan-images/bo/naga1.png";
+import imgWefinex from "dan-images/bo/wefinex1.png";
 
 const carouselData = [
-    {
-        title: "Wefinex",
-        link: "https://wefinex.net/reg?r=4ADE545",
-        img: imgBinance,
-    },
-    {
-        title: "NagaTrade",
-        link: "https://trade.nagatrade.com/authorize/register?ref=5ea284d2a2170",
-        img: imgNaga,
-    },
-    {
-        title: "Youtube",
-        youtube: true,
-        link: "https://www.youtube.com/embed/Zmh4oRhRh08",
-        img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
-    },
-    {
-        title: "Wefinex",
-        link: "https://wefinex.net/reg?r=4ADE545",
-        img: imgExness,
-    },
-    {
-        title: "Wefinex",
-        link: "https://wefinex.net/reg?r=4ADE545",
-        img: imgWefinex,
-    },
+  {
+    title: "Wefinex",
+    link: "https://www.google.com/",
+    img: imgBinance,
+  },
+  {
+    title: "NagaTrade",
+    link: "https://www.google.com/",
+    img: imgNaga,
+  },
+  {
+    title: "Youtube",
+    youtube: true,
+    link:
+      "https://www.youtube.com/embed/videoseries?list=PLJiGAeuHLIAZWhJwi-qnNOUp-8Tsc7Qw-",
+    img: "https://s3-ap-southeast-1.amazonaws.com/ex.otfund.io/static/naga.png",
+  },
+  {
+    title: "Wefinex",
+    link: "https://www.exness.com/a/uk4bt99m",
+    img: imgExness,
+  },
+  {
+    title: "Wefinex",
+    link: "https://wefinex.net/reg?r=4ADE545",
+    img: imgWefinex,
+  },
 ];
 const styles = {
-    item: {
-        textAlign: "center",
-        "& img": {
-            // margin: "10px auto",
-        },
+  item: {
+    textAlign: "center",
+    "& img": {
+      // margin: "10px auto",
     },
+  },
 };
 const GameLucky = ({ classes }) => {
-    const [link, setLink] = useState('')
-    return (
-        <Wrap className="container">
-            <div className="sort">
-                {
-                    carouselData.map((item, index) =>
-                        <div key={index.toString()} className={`${classes.item} item-${index}`} onClick={() => setLink(item.link)} >
-                            {
-                                item.youtube ?
-                                    <div className="video">
-                                        <Iframe
-                                            url={item.link}
-                                            frameBorder={0}
-                                            onClick={() => { setLink(item.link) }}
-                                        />
-                                    </div>
-                                    :
-                                    <img src={item.img} alt={item.title} />
-                            }
-                        </div>
-                    )
-                }
-            </div>
-            {!(link === '') &&
-                <Row>
-                    <Column>
-                        <Iframe
-                            url={link}
-                        />
-                    </Column>
-                </Row>
-            }
-        </Wrap>
-    );
-}
+  const [link, setLink] = useState("");
+  return (
+    <Wrap className="container">
+      <div className="sort">
+        {carouselData.map((item, index) => (
+          <div
+            key={index.toString()}
+            className={`${classes.item} item-${index}`}
+            onClick={() => setLink(item.link)}
+          >
+            {item.youtube ? (
+              <div className="video">
+                <Iframe
+                  url={item.link}
+                  frameBorder={0}
+                  onClick={() => {
+                    setLink(item.link);
+                  }}
+                />
+              </div>
+            ) : (
+              <img src={item.img} alt={item.title} />
+            )}
+          </div>
+        ))}
+      </div>
+      {!(link === "") && (
+        <Row>
+          <Column>
+            <Iframe url={link} />
+          </Column>
+        </Row>
+      )}
+    </Wrap>
+  );
+};
 
 GameLucky.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(GameLucky);
 const Wrap = styled.div`
-    .video{
-        border:2px solid #fff;
-        margin-top:0;
-        overflow: hidden;
-        padding-top: 56.25%;
-        position: relative;
-        iframe{
-            height: 100%;
-            left: 0;
-            position: absolute;
-            top: 0;
-            width: 100%;
-        }
+  .video {
+    border: 2px solid #fff;
+    margin-top: 0;
+    overflow: hidden;
+    padding-top: 56.25%;
+    position: relative;
+    iframe {
+      height: 100%;
+      left: 0;
+      position: absolute;
+      top: 0;
+      width: 100%;
     }
-    img{
-        border:2px solid #fff;
+  }
+  img {
+    border: 2px solid #fff;
+  }
+  .sort {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    .item-0,
+    .item-1 {
+      grid-row: 1;
     }
-    .sort{
-        display:grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        align-items:center;
-        .item-0,.item-1{
-            grid-row:1;
-        }
-        .item-2{
-            grid-area: 1/2/3;
-        }
-
+    .item-2 {
+      grid-area: 1/2/3;
     }
+  }
 `;
 
 const Row = styled.div`
@@ -128,20 +130,20 @@ const Row = styled.div`
   margin-top: 20px;
 `;
 const Column = styled.div`
+  width: 100%;
+  height: 100vh;
+  /* border:2px solid #fff; */
+  margin-top: 0;
+  overflow: hidden;
+  padding-top: 56.25%;
+  position: relative;
+  iframe {
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
     width: 100%;
-    height: 100vh;
-    /* border:2px solid #fff; */
-    margin-top:0;
-    overflow: hidden;
-    padding-top: 56.25%;
-    position: relative;
-    iframe{
-        height: 100%;
-        left: 0;
-        position: absolute;
-        top: 0;
-        width: 100%;
-    }
+  }
   .close {
     position: absolute;
     right: 0;
