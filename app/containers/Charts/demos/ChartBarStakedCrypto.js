43e8c14cdfec0fc3fa9d67a75ef9fb9a894253be
Louis <congnt@hvtc.edu.vn>
2020-05-29 12:00:29 +0700

    ađ

--- app/containers/Charts/demos/ChartBarStakedCrypto.js
+++ app/containers/Charts/demos/ChartBarStakedCrypto.js
@@ -31,7 +31,7 @@ const ChartBarStakedCrypto = ({ classes }) => {
   const textTran = (text) => {
     return i18n.exists(text) ? t(text) : text;
   };
-  const name = ["0.5$", "0.75$", "1$"];
+  const name = ["1$", "1$", "1$"];
   let chartData =
     chart && chart.order
       ? chart.order.map((item, index) => ({
